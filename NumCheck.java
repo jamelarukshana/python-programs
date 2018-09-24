86773fa5cc39bb16ee93ab0c092c7128df3cad44
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-09-24 13:52:17 +0530

    Update and rename Number.java to NumCheck.java

---
+++ NumCheck.java
@@ -0,0 +1,25 @@
+/*Positive or Negative or Zero*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class NumCheck
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int num;
+        //Scanner class to read value
+        Scanner sc=new Scanner(System.in);
+   //check condition for +ve, -ve and Zero
+        if(num>0)
+            System.out.println("Positive");
+        else if(num<0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+ 
+    }
+}
+ 
