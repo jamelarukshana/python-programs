944abe68e27f33e2a74d020de7654731c0efbd52
jamelarukshana <42768504+jamelarukshana@users.noreply.github.com>
2018-09-25 09:23:19 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,21 @@
+/*Positive or Negative or Zero */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Number
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int num;
+        Scanner sc=new Scanner(System.in);
+        if(num>0)
+            System.out.println("Positive");
+        else if(num<0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+ 
+    }
+}
